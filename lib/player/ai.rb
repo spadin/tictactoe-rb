606edd308887392e61bf87ac6dccea442762d588
Sandro Padin <sandropadin@gmail.com>
2012-12-17 10:20:07 -0600

    Removing @ symbols all over the place.

--- lib/player/ai.rb
+++ lib/player/ai.rb
@@ -1,9 +1,9 @@
 class AI
-  attr_reader   :marker
+  attr_reader   :player_marker
   attr_accessor :board
 
-  def initialize(marker)
-    @marker = marker
+  def initialize(player_marker)
+    @player_marker = player_marker
   end
 
   def move
@@ -12,21 +12,21 @@ class AI
   end
 
   def opponent_marker
-    @opponent_marker ||= (@marker == "x")? "o" : "x"
+    @opponent_marker ||= (player_marker == "x")? "o" : "x"
   end
 
   def calculate_move(current_comparison = :>)
     next_comparison = (current_comparison == :>)? :< : :>
     calculated_move, calculated_score = nil, nil
-    @board.free_positions.each do |position|
-      @board.mark(position, @marker) if current_comparison == :>
-      @board.mark(position, opponent_marker) if current_comparison == :<
-      if @board.gameover? 
-        score = get_score
+    board.free_positions.each do |position|
+      board.mark(position, player_marker) if current_comparison == :>
+      board.mark(position, opponent_marker) if current_comparison == :<
+      if board.gameover? 
+        score = calculate_score
       else
         move_position, score = calculate_move(next_comparison)
       end
-      @board.undo_last_mark!
+      board.undo_last_mark!
 
       if calculated_score == nil || score.send(current_comparison, calculated_score)
         calculated_move, calculated_score = position, score
@@ -36,10 +36,10 @@ class AI
     [calculated_move, calculated_score]    
   end
 
-  def get_score
-    if @board.winner.nil?
+  def calculate_score
+    if board.winner.nil?
       0
-    elsif @board.winner == @marker
+    elsif board.winner == player_marker
       1
     else
       -1
