2cdbffb188175d94f34448589c94d31b73731b9a
Sandro Padin <sandropadin@gmail.com>
2013-01-04 13:59:45 -0600

    No longer monkey patching the String class.

--- lib/player/human.rb
+++ lib/player/human.rb
@@ -10,16 +10,16 @@ class Human
   end
 
   def prompt_for_move(move='')
-    until move.numeric? && move.to_i >= 0 && move.to_i < 10
+    until numeric?(move) && move.to_i >= 0 && move.to_i < 10
       puts "#{player_marker}'s turn, what's your next move?"
       move = gets.chomp
     end
     move.to_i
   end
-end
 
-class String
-  def numeric?
-    Integer(self) != nil rescue false
+  private
+
+  def numeric?(move)
+    Integer(move) != nil rescue false
   end
 end
